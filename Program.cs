e15a5218b8cf6aefff84f09e8bebe8761bc22138
pespaA <omaro@OMARMOKHTAR>
2024-12-17 17:27:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,198 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Transactions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace session5
+{
+    internal class Program
+    {
+
+        #region 01 The Method
+        static void ValueTypeParametersByValue(int x)
+        {
+            x = 100;
+        }
+        static void ValueTypeParametersByReference(ref int x)
+        {
+            x = 100;
+        }
+        #endregion
+        #region 02 The Method
+        static void ReferenceTypeParametersByValue(int[] x)
+        {
+            x[0] = 100;
+        }
+        static void ReferenceTypeParametersByReference(ref int[] x)
+        {
+            x[0] = 100;
+        }
+        #endregion
+        #region 03 The Method
+        static void SumAndSub(int num1,int num2,out int sum,out int sub)
+        {
+            sum = num1 + num2;
+            sub = num1 - num2;
+        }
+        #endregion
+        #region 04 The Method
+        static int sumOfGivenNumber(string x)
+        {
+            int sum = 0;
+            for (int i = 0;i < x.Length;i++)
+            {
+                sum += int.Parse(x[i].ToString());
+            }
+            return sum;
+        }
+        #endregion
+        #region 05 The Method
+        static bool IsPrime(int x)
+        {
+            if (x < 2)
+                return false;
+            for (int i = 2; i <= Math.Sqrt(x); i++)
+            {
+                if (x % i == 0)
+                    return false; 
+            }
+            return true;
+        }
+        #endregion
+        #region 06 The Method
+        static void MinMaxArray(int[] arr,out int min,out int max)
+        {
+            min = arr.Min();
+            max = arr.Max();
+            
+        }
+        #endregion
+        #region 07 The Method
+        //static int factorial(int x)
+        //{
+        //    int sum = 1;
+        //    for (int i = x;i > 0;i--)
+        //    {
+        //        sum *= i;
+        //    }
+        //    return sum ;
+        //}
+        #endregion
+        #region 08 The Method
+        static string ChangeChar(string str , char pos , char replc)
+        {
+            return str.Replace(pos, replc);
+        }
+        #endregion
+        
+        static void Main(string[] args)
+        {
+            demo.Demo0();
+
+            //Assignment 4
+            //Functions
+            #region 01
+            ////Explain the difference between passing (Value type parameters)
+            ////by value and by reference
+            ////then write a suitable c# example?
+            /////////////////////////////////////////////////
+            ////1. Passing by Value
+            ////1- copy of the value is passed to the method.
+            ////2- Any changes made to the parameter inside the method
+            ////do not affect the original variable.
+
+            ////2. Passing by Reference
+            ////1- Changes made to the parameter inside the method affect the original variable.
+
+            ////ex
+            //int x = 0;
+            //Console.WriteLine("before call: "+ x);
+            //ValueTypeParametersByValue(x);
+            //Console.WriteLine("After call: " + x);
+
+            //Console.WriteLine("Value Type Parameters By Reference");
+
+            //int y = 0;
+            //Console.WriteLine("before call: " + y);
+            //ValueTypeParametersByReference(ref y);
+            //Console.WriteLine("After call: " + y);
+            #endregion
+
+            #region 02
+            ////Explain the difference between passing (Reference type parameters)
+            ////by value and by reference then write a suitable c# example?
+
+            /////////////////////////////////////////////////
+            ////1. Passing by Value
+            ////1- A copy of the reference (not the object itself) is passed to the method.
+            ////2- The method can modify the object the reference points to, but it cannot change
+            ////the reference to point to a new object.
+
+            ////2. Passing by Reference
+            ////1- The reference itself is passed to the method, not just a copy of the reference.
+            ////2- The method can modify both the object's properties and reassign the reference to a new object.
+
+            ////ex
+            //int[] x = { 0,1 };
+            //Console.WriteLine("before call: " + x[0]);
+            //ReferenceTypeParametersByValue(x);
+            //Console.WriteLine("After call: " + x[0]);
+
+            //Console.WriteLine("Reference Type Parameters By Reference");
+
+            //int[] y = { 0, 1 };
+            //Console.WriteLine("before call: " + y[0]);
+            //ReferenceTypeParametersByReference(ref y);
+            //Console.WriteLine("After call: " + y[0]);
+            #endregion
+
+            #region 03
+            //Console.WriteLine("Enter 2 numbers");
+            //int x= int.Parse(Console.ReadLine());
+            //int y= int.Parse(Console.ReadLine());
+            //int sum, sub;
+            //SumAndSub(x, y, out sum, out sub);
+            //Console.WriteLine($"summation is: {sum} , subtractin is: {sub}");
+            #endregion
+
+            #region 04
+            //Console.WriteLine("enter number:");
+            //string num = Console.ReadLine();
+            //int sum = sumOfGivenNumber(num);
+            //Console.WriteLine($"The sum of the digits of the number {num} is: {sum}");
+            #endregion
+
+            #region 05
+            //Console.WriteLine("Enter number");
+            //int num = int.Parse(Console.ReadLine());
+            //Console.WriteLine(IsPrime(num));
+            #endregion
+
+            #region 06
+            //int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+            //int min ,max ;
+            //MinMaxArray(arr,out min,out max);
+            //Console.WriteLine($"The min is: {min} , The max is: {max}");
+
+            #endregion
+
+            #region 07
+            //Console.WriteLine("Enter number to get factorial:");
+            //int input = int.Parse(Console.ReadLine());
+            //int sum = factorial(input);
+            //Console.WriteLine($"The factorial for number {input} is {sum}");
+            #endregion
+
+            #region 08
+            //Console.WriteLine("Enter string: ");
+            //string input = Console.ReadLine();
+            //Console.WriteLine("Enter char to delete: ");
+            //char cha1 = char.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter char to replac: ");
+            //char cha2 = char.Parse(Console.ReadLine());
+
+            //Console.WriteLine(ChangeChar(input,cha1,cha2));
+
+            #endregion
+        }
+    }
+}
---
+++ demo.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace session5
+{
+    internal class demo
+    {
+        //static Functions (Class Member Function)
+        //Object Member Function
+        public static void swap(ref int x,ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+            
+        }
+
+        public static void SumMul(int x,int y,out int sum,out int mul )
+        {
+            sum = x + y;
+            mul = x* y;
+        }
+
+        public static int Sumarray(params int[] arr)//passing By value
+        {
+            int sum = 0;
+            if (arr is not null)
+            {
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    sum += arr[i];//sum = sum + arr[i]
+                }
+            }
+            return sum;
+        }
+
+        public static void DoSomeCode()
+        {
+            try
+            {
+                int x, y, z;
+                bool flag;
+                //x = int.Parse(Console.ReadLine());
+                //y = int.Parse(Console.ReadLine());
+                do
+                {
+                    Console.WriteLine("pls Enter Number 1");
+                    flag=int.TryParse(Console.ReadLine(), out x);
+                }while (!flag);
+                do
+                {
+                    Console.WriteLine("pls Enter Number 2");
+                    flag = int.TryParse(Console.ReadLine(), out y);
+                } while (!flag || y==0);
+                z = x / y;
+                //null Propagetion Opertor
+                int[] numbers = default;// size 3
+                if (numbers?.Length>10)
+                {
+                    numbers[10] = 100;
+                }
+                
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public static void Demo0()
+        {
+            #region Judged Array
+            //// judged array => One D array of Arrays With Differnet Sizes
+            //int[][] Numbers = new int[3][];
+            //Numbers[0] = new int[] {1,2,3};
+            //Numbers[1] = new int[] {4,5};
+            //Numbers[2] = new int[] {6};
+            ////print
+            //for (int i = 0; i < 3; i++)
+            //{
+            //    for (int j = 0; j < Numbers[i].Length; j++)
+            //        Console.WriteLine($"Numbers[{i}][{j}]:"+Numbers[i][j]);
+            //}
+            #endregion
+
+            #region passing By Value
+            //int a = 4,b = 5;
+            //Console.WriteLine($"a="+a);
+            //Console.WriteLine($"b=" + b);
+            //swap(ref a,ref b);
+            //Console.WriteLine($"a=" + a);
+            //Console.WriteLine($"b=" + b);
+            #endregion
+
+            #region Passing By Out
+            //int a = 3, b = 5, sumresult, mulreselt;
+            //SumMul(a,b,out sumresult,out mulreselt);
+            //Console.WriteLine($"The sum={sumresult}");
+            //Console.WriteLine($"The mul={mulreselt}");
+            #endregion
+
+            #region Params
+            ////int[] Numbers = { 1 , 2 , 3 , 4, 5 };
+            ////int Total = Sumarray(Numbers);
+            //int Total = Sumarray(1,2,3,4,5);
+            //Console.WriteLine("The sum is: "+Total);
+            #endregion
+
+            #region boxing vs unboxing
+            ////Boxing => casting  From Value Type To Refernce Type
+            ////UnBoxing => casting  From Refernce Type
+            ////object o1;
+            ////o1 = "Omar";
+            ////o1 = 3;   //Boxing( Value Type => Refernce Type )
+
+            //int x = 10 ;
+            //object obj = x ;  // Boxing [Safe Casting]
+            //                 //Parent => Chiled
+            //                //Animal => Dog (Dog is an Animal)
+            //// ========================
+
+            //object obj2 = "Omar" ;
+            //int y = (int)obj2; // Unboxing [Unsafe]
+            //                  //Dog = animal ( Animal is Dog)
+
+            //Console.WriteLine(y);
+
+            #endregion
+
+            #region Nullable Oprator ?
+
+            #region Value Type
+            //// Value Type => Doesnot  allow Null
+            //// Reference Type => Allow Null
+            ////-----------------------------------------
+            //// c#2 2005
+            ////int num = null;
+            ////int? num2 = null; // Allow Null or Value
+            ////int x = 10;
+            ////int? y = x;
+            ////-----------------------------------------
+            //int? x = 10;// Syntax Suger
+            //Nullable<int> num1; // int? num1
+            ////int y = x;// invalid
+            //float? num2; 
+            #endregion
+            #region Refence Type
+            //Refence Type => Allow Null
+            string? Name = "Omar";
+            Name = null;
+            // Null Forgivness Operator
+            //Name = null!;
+            #endregion
+            #endregion
+
+            #region Null Propagation Oprator
+            //int[] Numbers=default;
+            //for (int i = 0;/*(Numbers is not null) &&*/ (i < Numbers?.Length);i++)
+            //{
+            //    Console.WriteLine(Numbers[i]);
+            //}
+            //Console.WriteLine(6);
+            #endregion
+
+            #region Null Coaleasing Oprator
+            //int[] Numbers = default;
+            //for (int i = 0;i < Numbers?.Length??0; i++)
+            //{
+            //    Console.WriteLine(Numbers[i]);
+            //}
+            //Console.WriteLine(6);
+            #endregion
+
+            //try
+            //{
+            //    DoSomeCode();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex);
+            //}
+            //finally
+            //{
+            //    //close / Delete / Realse / Dellocate / Free Unreachable Resources
+
+            //    // Connection With Detabase
+            //}
+        }
+    }
+}
